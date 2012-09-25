eb673efb98a88b3794a1e646d93485a5c6ace291
Chetan Sarva <chetan@pixelcop.net>
2012-09-24 21:29:12 -0400

    fixed install script for ubuntu

--- bin/install.sh
+++ bin/install.sh
@@ -2,10 +2,21 @@
 
 set -e
 
-aptitude -y install build-essential ruby rubygems curl libcurl4-openssl-dev
+# install deps
+apt-get -yqq install build-essential ruby rubygems curl libcurl4-openssl-dev libopenssl-ruby > /dev/null
+
+# grab bixby
+wget -q http://192.168.80.99/~chetan/bixby/bixby-agent.tar.gz
 tar -xzf bixby-agent.tar.gz
 cd bixby/
-bin/bundle install --deployment --local --without development test
+
+# fix rubygems
+gem install -q vendor/cache/rubygems-update*.gem --no-ri --no-rdoc > /dev/null
+ruby /var/lib/gems/1.8/gems/rubygems-update-*/setup.rb > /dev/null
+
+# install gems
+bin/bundle install --deployment --local --without development test > /dev/null
+
+# move into place
 cd ..
 mv bixby /opt/
-cd /opt/bixby/
