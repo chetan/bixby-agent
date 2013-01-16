67621d51a116d34d853586e2b5961ede67969c86
Chetan Sarva <chetan@pixelcop.net>
2013-01-15 22:45:36 -0500

    no longer ship vendored bundler; set encoding to utf-8

--- bin/agent.rb
+++ bin/agent.rb
@@ -1,17 +1,8 @@
 #!/usr/bin/env ruby
+# encoding: utf-8
 
 require 'rubygems'
-
-begin
-  require "bundler"
-rescue Exception => ex
-  if not Object.const_defined? :Bundler then
-    # load from vendored path
-    require 'pathname'
-    $: << File.join(File.expand_path(File.dirname(Pathname.new(__FILE__).realpath)), "../vendor/cache/bundler-1.2.1/lib")
-    require "bundler"
-  end
-end
+require 'bundler'
 Bundler.setup(:default)
 
 $: << File.expand_path(File.join(File.dirname(__FILE__), "../lib"))
