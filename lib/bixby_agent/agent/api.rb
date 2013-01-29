4ee2d1f0897cc721b975e1b769657b7ce9d37f37
Chetan Sarva <chetan@pixelcop.net>
2013-01-29 14:15:20 -0500

    sign downloads as well; added some helpers for api calls

--- lib/bixby_agent/agent/api.rb
+++ lib/bixby_agent/agent/api.rb
@@ -7,26 +7,34 @@ class Agent
 
     # Execute the given API request on the manager
     #
+    # @param [String] operation  Name of operation
+    # @param [Array] params  Array of parameters; must ve valid JSON types
+    def api_call(op, params)
+      exec_api(JsonRequest.new(op, params))
+    end
+
+    # Execute the given API download request
+    #
+    # @param [JsonRequest] json_req     Request to download a file
+    # @param [String] download_path     Absolute filename to download requested file to
+    # @return [JsonResponse]
+    def exec_download(download_path, op, params)
+      exec_api_download(JsonRequest.new(op, params), download_path)
+    end
+
+    # Execute the given API request on the manager
+    #
     # @param [JsonRequest] json_req
     # @return [JsonResponse]
     def exec_api(json_req)
-      uri = URI.join(Bixby.manager_uri, "/api").to_s
       begin
-        post = json_req.to_json
-        if crypto_enabled? and have_server_key? then
-          req = HTTPI::Request.new(:url => uri, :body => post)
-          req.headers["Content-Type"] = "application/json"
-          ApiAuth.sign!(req, access_key, secret_key)
-          res = HTTPI.post(req).body
-        else
-          res = json_req.http_post_json(uri, post)
-        end
+        req = sign_request(json_req)
+        res = HTTPI.post(req).body
         return JsonResponse.from_json(res)
       rescue Curl::Err::CurlError => ex
         return JsonResponse.new("fail", ex.message, ex.backtrace)
       end
     end
-
 
     # Execute the given API download request
     #
@@ -34,17 +42,40 @@ class Agent
     # @param [String] download_path     Absolute filename to download requested file to
     # @return [JsonResponse]
     def exec_api_download(json_req, download_path)
-      uri = URI.join(Bixby.manager_uri, "/api").to_s
       begin
-        post = json_req.to_json
-        if crypto_enabled? and have_server_key? then
-          post = encrypt_for_server(post)
+        req = sign_request(json_req)
+        File.open(download_path, "w") do |io|
+          req.on_body { |d| io << d; d.length }
+          HTTPI.post(req)
         end
-        json_req.http_post_download(uri, post, download_path)
         return JsonResponse.new("success")
       rescue Curl::Err::CurlError => ex
         return JsonResponse.new("fail", ex.message, ex.backtrace)
       end
+    end
+
+
+    private
+
+    # Create a signed request
+    #
+    # @param [JsonRequest] json_req
+    #
+    # @return [HTTPI::Request]
+    def sign_request(json_req)
+      post = json_req.to_json
+      req = HTTPI::Request.new(:url => api_uri, :body => post)
+      req.headers["Content-Type"] = "application/json"
+
+      if crypto_enabled? and have_server_key? then
+        ApiAuth.sign!(req, access_key, secret_key)
+      end
+
+      return req
+    end
+
+    def api_uri
+      URI.join(Bixby.manager_uri, "/api").to_s
     end
 
   end # API
