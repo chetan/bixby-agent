5e8f9eed14afa5d4bf816e42965eb1cd6101015c
Chetan Sarva <chetan@pixelcop.net>
2013-02-07 12:21:57 -0500

    only set home if unset

--- lib/bixby_agent.rb
+++ lib/bixby_agent.rb
@@ -2,7 +2,7 @@
 # try to detect dev env
 path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 if File.directory? File.join(path, ".git") then
-  ENV["BIXBY_HOME"] = path
+  ENV["BIXBY_HOME"] = path if not ENV["BIXBY_HOME"]
   require "bundler/setup"
 end
 
