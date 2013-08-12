016c132c7079522e5a838546ca4f48cd6032fa94
Chetan Sarva <chetan@pixelcop.net>
2013-08-12 17:05:30 -0400

    remove duplicate lib file

--- lib/bixby-agent.rb
+++ lib/bixby-agent.rb
@@ -1,1 +1,15 @@
-require "bixby_agent"
+
+# try to detect dev env
+path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+if File.directory? File.join(path, ".git") then
+  ENV["BIXBY_HOME"] = path if not ENV["BIXBY_HOME"]
+  require "bundler/setup"
+end
+
+require "bixby_common"
+require "bixby-client"
+
+require "bixby_agent/agent"
+require "bixby_agent/version"
+
+Bixby::Agent.setup_env()
