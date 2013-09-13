03b39efa50779db4157649c8906fc8f2c23dd83c
Chetan Sarva <chetan@pixelcop.net>
2013-09-13 18:55:10 -0400

    added WebSocket::Client from common

---
+++ lib/bixby-agent/websocket/client.rb
@@ -0,0 +1,134 @@
+
+require 'faye/websocket'
+require 'eventmachine'
+
+module Bixby
+  module WebSocket
+
+    # WebSocket Client
+    class Client
+
+      include Bixby::Log
+
+      attr_reader :ws, :api
+
+      def initialize(url, handler)
+        @url = url
+        @handler = handler
+        @tries = 0
+        @exiting = false
+      end
+
+      # Start the Client thread
+      #
+      # NOTE: This call never returns!
+      def start
+
+        @exiting = false
+
+        Kernel.trap("EXIT") do
+          # :nocov:
+          @exiting = true
+          # :nocov:
+        end
+
+        logger.debug "connecting to #{@url}"
+        EM.run {
+          connect()
+        }
+      end
+
+      def stop
+        @exiting = true
+        EM.stop_event_loop if EM.reactor_running?
+      end
+
+
+      private
+
+      # Connect to the WebSocket endpoint given by @url. Will attempt to keep
+      # the connection open forever, reconnecting as needed.
+      def connect
+
+        @ws = Faye::WebSocket::Client.new(@url, nil, :ping => 60)
+        @api = Bixby::WebSocket::APIChannel.new(@ws, @handler)
+
+        ws.on :open do |e|
+          begin
+            logger.info "connected to manager at #{@url}"
+            api.open(e)
+            @tries = 0
+
+            # send a connection request
+            id = SecureRandom.uuid
+            json_req = JsonRequest.new("", "")
+            signed_req = SignedJsonRequest.new(json_req, Bixby.agent.access_key, Bixby.agent.secret_key)
+            connect_req = Request.new(signed_req, id, "connect")
+            EM.next_tick {
+              ws.send(connect_req.to_wire)
+            }
+
+          rescue Exception => ex
+            logger.error ex
+          end
+        end
+
+        ws.on :message do |e|
+          begin
+            api.message(e)
+          rescue Exception => ex
+            logger.error ex
+          end
+        end
+
+        ws.on :close do |e|
+          begin
+            if api.connected? then
+              logger.info "lost connection to manager"
+            else
+              logger.debug "failed to connect"
+            end
+            api.close(e)
+            if backoff() then
+              connect()
+            end
+          rescue Exception => ex
+            logger.error ex
+          end
+        end
+      end
+
+      # Delay reconnection by a slowly increasing interval
+      def backoff
+
+        if @exiting or not EM.reactor_running? then
+          # shutting down, don't try to reconnect
+          puts "not retrying"
+          return false
+        end
+
+        @tries += 1
+        if @tries == 1 then
+          logger.debug "retrying immediately"
+
+        # :nocov:
+        elsif @tries == 2 then
+          logger.debug "retrying every 1 sec"
+          sleep 1
+        elsif @tries <= 30 then
+          sleep 1
+        elsif @tries == 31 then
+          logger.debug "retrying every 5 sec"
+          sleep 5
+        else
+          sleep 5
+        end
+        # :nocov:
+
+        true
+      end
+
+    end
+
+  end
+end
---
+++ test/websocket/test_client.rb
@@ -0,0 +1,117 @@
+
+require 'helper'
+
+module EventMachine
+  class << self
+
+    alias_method :_orig_run, :run
+    alias_method :_orig_next_tick, :next_tick
+    alias_method :_orig_stop, :stop
+    alias_method :_orig_stop_event_loop, :stop_event_loop
+
+    def run_immediately(&block)
+      @reactor_running = true
+      block.call() if block
+    end
+
+    def tick_immediately(&block)
+      block.call() if block
+    end
+
+    def noop
+    end
+
+    def stub!
+      define_singleton_method :run, method(:run_immediately)
+      define_singleton_method :next_tick, method(:tick_immediately)
+      define_singleton_method :stop, method(:noop)
+      define_singleton_method :stop_event_loop, method(:noop)
+    end
+
+    def disable_stub!
+      define_singleton_method :run, method(:_orig_run)
+      define_singleton_method :next_tick, method(:_orig_next_tick)
+      define_singleton_method :stop, method(:_orig_stop)
+      define_singleton_method :stop_event_loop, method(:_orig_stop_event_loop)
+    end
+
+  end
+end
+
+module Bixby
+module Test
+
+  class TestHandler < Bixby::RpcHandler
+  end
+
+module WebSocket
+class TestClient < TestCase
+
+  class FakeClient
+    attr_reader :types
+    def initialize
+      @types = {}
+    end
+    def on(type, &block)
+      @types[type] = block
+    end
+    def trigger(type, event)
+      @types[type].call(event)
+    end
+  end
+
+  def setup
+    super
+    EM.stub!
+    setup_existing_agent()
+  end
+
+  def teardown
+    EM.disable_stub!
+  end
+
+  def test_connect_and_reconnect
+
+    @fake_ws = FakeClient.new
+    # created twice due to reconnect
+    Faye::WebSocket::Client.expects(:new).with("http://localhost", nil, :ping => 60).returns(@fake_ws).times(2)
+    @client = Bixby::WebSocket::Client.new("http://localhost", TestHandler)
+    @client.start
+
+    assert @fake_ws.types[:open]
+    assert @fake_ws.types[:message]
+    assert @fake_ws.types[:close]
+
+    # connect
+    @fake_ws.expects(:send).with{ |r| c = Bixby::WebSocket::Message.from_wire(r); c.type == "connect" }.times(2)
+    @fake_ws.trigger(:open, nil)
+    assert @client.api.connected?
+
+    # reconnect
+    @fake_ws.trigger(:close, nil)
+    refute @client.api.connected?
+
+    # stop w/o backoff/reconnect
+    @fake_ws.trigger(:open, nil)
+    assert @client.api.connected?
+
+    EM.expects(:stop_event_loop).once()
+    @client.stop()
+    @fake_ws.trigger(:close, nil)
+    refute @client.api.connected?
+  end
+
+  def test_message
+    @fake_ws = FakeClient.new
+    Faye::WebSocket::Client.expects(:new).with("http://localhost", nil, :ping => 60).returns(@fake_ws)
+    @client = Bixby::WebSocket::Client.new("http://localhost", TestHandler)
+    @client.start
+
+    @client.api.expects(:message).once.with("foobar")
+    @fake_ws.trigger(:message, "foobar")
+  end
+
+end
+end
+end
+end
