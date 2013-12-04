d82b375697292929a439cb0005b2ec8724eff1e3
Chetan Sarva <chetan@pixelcop.net>
2013-12-04 16:41:13 -0500

    set log level directly

--- lib/bixby-agent/app.rb
+++ lib/bixby-agent/app.rb
@@ -100,7 +100,7 @@ class App
   # NOTE: this call will not return!
   def start_websocket_client
     # make sure log level is still set correctly here
-    Bixby::Log.setup_logger(:level => Logging.appenders["file"].level)
+    Logging.logger.root.level = Logging.appenders["file"].level
     @client = Bixby::WebSocket::Client.new(Bixby.agent.manager_ws_uri, AgentHandler)
     @client.start
   end
